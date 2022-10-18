9cb115de16ee44def84a988d08c044ca4a752392
Max Nabokow <max.nabokow@gmail.com>
2022-10-18 15:25:26 +0800

    Initial Commit

---
+++ Fridge/FridgeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FridgeApp.swift
+//  Fridge
+//
+//  Created by Max Nabokow on 10/18/22.
+//
+
+import SwiftUI
+
+@main
+struct FridgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
